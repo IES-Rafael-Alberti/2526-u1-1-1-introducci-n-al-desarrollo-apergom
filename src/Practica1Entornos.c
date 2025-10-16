18dd7564165f110b39f8f5f61a070486cfb40a9a
apergom <aperezg@iesrafaelalberti.es>
2025-10-16 19:15:56 +0200

    Añadido programa en C y capturas del funcionamiento de ambos

---
+++ src/Practica1Entornos.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    char nombre[50];
+    int nacimiento, edad;
+
+    printf("Programa mensaje en C\n");
+
+    printf("¿Cómo se llama?\n ");
+    scanf("%49s", nombre);
+
+    printf("¿En qué año nació?\n ");
+    scanf("%d", &nacimiento);
+
+    edad = 2025 - nacimiento;
+    printf("[translate:Hola %s, tienes %d años. Este programa está hecho en el lenguaje de programación: C]", nombre, edad);
+
+    return 0;
+}
